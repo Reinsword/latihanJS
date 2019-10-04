21ac24479e0ca6478ce5c28c0264857f0d46434d
Reinsword <muhamadhariyanto.2019@student.uny.ac.id>
2019-10-04 17:22:42 +0700

    penjumlahan

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 $('#banner').text("Hello, JQuery!");
 
-function P() {
-    var nama = $('#nama').val();
-    $('#banner').text(`Assalamualaikum ${nama}, gak jawab DPR!`);
+function tambahkan() {
+    var angka_pertama = parseInt($('#angka_pertama').val());
+    var angka_kedua = parseInt($('#angka_kedua').val());
+
+    var hasil = angka_pertama + angka_kedua;
+
+    $('#hasil').val(hasil);
 }
